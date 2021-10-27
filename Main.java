2feb8991fefd51ca8cf2c610266d5ee36464d940
mahmuda-17 <mahmudanirjona63@gmail.com>
2021-10-27 14:08:47 +0600

    first commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+package oop;
+
+
+public class Main {
+    public static void main(String[] args){
+
+       int  x = 9;
+       int  y = 10;
+       if(x>y){
+        System.out.println("x is greater[x,y]");}
+       else
+        {
+            System.out.println( "x is smaller[x,y]");
+        }
+
+    }
+}
+
+
+
